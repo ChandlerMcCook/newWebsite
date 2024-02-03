11b3c131472e0fb11f2b1db602dcc5e76a40f99c
ChandlerMcCook <chandler.mccook@gmail.com>
2024-02-03 15:23:06 -0600

    Tiles have functionality. Started working on Map and Tile class and map generation

--- test/src/components/Field.tsx
+++ test/src/components/Field.tsx
@@ -3,6 +3,7 @@ import MineClick from "./Mine";
 
 const verticalAxis = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
 const horizontalAxis = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i'];
+// const minePicture = require("../assets/images/piggity.png")
 
 export default function Field() {
     let board = [];
@@ -10,8 +11,12 @@ export default function Field() {
     for (let i=0; i<horizontalAxis.length; i++) {
         for (let j=0; j<verticalAxis.length; j++) {
             board.push(
-                <div className="tile">
-                    <button className="mineButton" onClick={MineClick}>{horizontalAxis[i]}{verticalAxis[j]}</button>
+                <div className="spot">
+                    <button id={horizontalAxis[i]+verticalAxis[j]} 
+                            className="mineButton" 
+                            onClick={e => MineClick(e.currentTarget.id, ('true' === e.currentTarget.value))}
+                            value="true">
+                    </button>
                 </div>
             );
         }
--- test/src/components/Mine.tsx
+++ test/src/components/Mine.tsx
@@ -1,5 +1,11 @@
 
-
-export default function MineClick() {
-    <div>asl;dhfj</div>
+export default function MineClick(id: string, mine : boolean) {
+    let slot = document.getElementById(id);
+    if (slot) {
+        if(mine) {
+            slot.style.backgroundColor = "red";
+        } else {
+            slot.style.backgroundColor = "#45db24";
+        }
+    }
 };
