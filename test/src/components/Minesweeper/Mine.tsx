446d4fbfb5f713da48fdfa245f7a2c31782b3ec5
ChandlerMcCook <chandler.mccook@gmail.com>
2024-02-06 23:07:48 -0600

    commented out flood fill

--- test/src/components/Minesweeper/Mine.tsx
+++ test/src/components/Minesweeper/Mine.tsx
@@ -3,19 +3,19 @@ import Tile from './TileClass'
 
 let board : TileMap = new TileMap(9, 9, 10);
 
-function FloodFill(id: string) {
-    let i = Number(id[0]);
-    let j = Number(id[1]); 
+// function FloodFill(id: string) {
+//     let i = Number(id[0]);
+//     let j = Number(id[1]); 
 
-    for (let a=i-1; a<(i+2); a++) {
-        if ((a > 0) && (a <= board.getHeight())) {
-            for (let b=j-1; b<(j+2); b++) {
-                if ((b > 0) && (b <= board.getWidth())) {
-                }
-            }
-        }
-    }
-}
+//     for (let a=i-1; a<(i+2); a++) {
+//         if ((a > 0) && (a <= board.getHeight())) {
+//             for (let b=j-1; b<(j+2); b++) {
+//                 if ((b > 0) && (b <= board.getWidth())) {
+//                 }
+//             }
+//         }
+//     }
+// }
 
 export default function MineClick(id: string) {
     let slot = document.getElementById(id);
@@ -25,9 +25,9 @@ export default function MineClick(id: string) {
         if(t.isMine()) {
             slot.style.backgroundColor = "red";
         } else {
-            if (t.getTouching() === 0) {
-                FloodFill(id);
-            }
+            // if (t.getTouching() === 0) {
+            //     FloodFill(id);
+            // }
             slot.style.backgroundColor = "#45db24";
             slot.innerHTML = String(t.getTouching());
         }
