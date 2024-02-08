ad380ad5e73943419d3a7da8cb3d2450ba4a46ea
ChandlerMcCook <chandler.mccook@gmail.com>
2024-02-07 21:04:19 -0600

    Adding other pages and Routers

--- test/src/App.tsx
+++ test/src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import {Routes, Route} from 'react-router-dom';
 import './App.css';
 import Field from './components/Minesweeper/Field'
+import Home from './components/homepage/home'
 
 function App() {
   return (
     <div id="app">
-      <Field/>
+      <Routes>
+        <Route path="/" element = {<Home></Home>} />
+      </Routes>
     </div>
   );
 }
