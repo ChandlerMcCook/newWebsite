0155b71e39f13f503a6378ea46d553f9b10185f5
ChandlerMcCook <chandler.mccook@gmail.com>
2024-02-07 18:27:12 -0600

    Trying to get flag to work

--- test/src/components/Minesweeper/Field.tsx
+++ test/src/components/Minesweeper/Field.tsx
@@ -1,5 +1,6 @@
 import './Field.css';
-import MineClick from './Mine';
+import MineClick from './Click';
+import Flag from './Flag';
 
 const verticalAxis = ['0', '1', '2', '3', '4', '5', '6', '7', '8'];
 const horizontalAxis = ['0', '1', '2', '3', '4', '5', '6', '7', '8'];
@@ -12,9 +13,11 @@ export default function Field() {
         for (let j=0; j<verticalAxis.length; j++) {
             board.push(
                 <div className="spot">
-                    <button id={horizontalAxis[i]+verticalAxis[j]} 
+                    <button type = "button"
+                            id={horizontalAxis[i]+verticalAxis[j]} 
                             className="mineButton" 
-                            onClick={e => MineClick(e.currentTarget.id)}>
+                            onClick={e => MineClick(e.currentTarget.id)}
+                            onContextMenu={e=>Flag(e.currentTarget.id)}>
                     </button>
                 </div>
             );
---
+++ test/src/components/Minesweeper/Flag.tsx
@@ -0,0 +1,7 @@
+export default function Flag(id : string) {
+    let slot = document.getElementById(id);
+
+    if (slot) {
+        slot.style.backgroundImage = '../../assets/images/pige.jpg';
+    }
+}
